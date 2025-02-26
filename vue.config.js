7ba49e7178575cff231211a5f4ee27b2338629a4
yamini-fero <yamini.patel@fero.ai>
2025-02-26 18:34:15 +0530

    modify vue config file

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
       ? '/vue-project/' // replace 'my-project' with your repo name
-      : '/',
+      : '/vue-project/',
 }
